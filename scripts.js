96991dfb0740fefdeed12127072e2758d3bbf428
Guillermo Orio Fernández <a22guillermoof@lnxd-i1e07.ateis.local>
2023-10-05 13:10:13 +0200

    Modificado: estructura del html. proporciones de las imagenes. estructura del script. Añadido: estilos completos

--- scripts.js
+++ scripts.js
@@ -3,31 +3,43 @@ function showData(datosJson) {
     const main = document.getElementById('main');
     datosJson.forEach(item => {
         const table = document.createElement('table');
-        const titleRow = document.createElement('th')
-        const titleCell = document.createElement('td');
+        const thead = document.createElement('thead');
+        const titleRow = document.createElement('tr')
+        const titleCell = document.createElement('th');
         titleCell.innerHTML = item.title;
+        titleCell.colspan = "2";
         const imageRow = document.createElement('tr');
         const imageCell = document.createElement('td')
+        imageCell.colspan= "2";
         const image = document.createElement('img')
         image.src = item.image
+        const tbody = document.createElement('tbody');
         const definitionRow = document.createElement('tr');
+        const definitionTitleCell = document.createElement('th');
+        definitionTitleCell.innerHTML = "Definición";
         const definitionCell = document.createElement('td');
         definitionCell.innerHTML = item.definition;
 
-        table.appendChild(titleRow);
+        table.appendChild(thead);
+        thead.appendChild(titleRow);
         titleRow.appendChild(titleCell)
-        table.appendChild(imageRow);
+        thead.appendChild(imageRow);
         imageRow.appendChild(imageCell)
         imageCell.appendChild(image)
-        table.appendChild(definitionRow);
+        table.appendChild(tbody);
+        tbody.appendChild(definitionRow);
         definitionRow.appendChild(definitionCell)
+        definitionRow.appendChild(definitionTitleCell)
 
         if (item.ampliar) {
             const AmpliarRow = document.createElement('tr');
+            const AmpliarTitleCell = document.createElement('th');
+            AmpliarTitleCell.innerHTML = "Para ampliar";
             const AmpliarCell = document.createElement('td');
             AmpliarCell.innerHTML = item.ampliar;
-            table.appendChild(AmpliarRow);
+            tbody.appendChild(AmpliarRow);
             AmpliarRow.appendChild(AmpliarCell)
+            AmpliarRow.appendChild(AmpliarTitleCell)
         }
 
         main.appendChild(table);
